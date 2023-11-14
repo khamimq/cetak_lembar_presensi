4c6f45dd35ddfab76a3a7aade79e79a70a338625
khamimq <khamim.q@gmail.com>
2023-11-14 08:06:14 +0700

    cetak presensi

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "db_sekolah_xirpl";
+    //koneksi ke database
+    $conn = mysqli_connect($host,$user,$pass,$database);
+?>
